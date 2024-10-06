c286e147b422cd696b59e0676dcd186f9e939360
Travis Swift <travis.arthur.swift@gmail.com>
2024-10-06 09:08:13 -0500

    fixed typos

---
+++ assets/js/project1.js
@@ -0,0 +1,32 @@
+"use strict";
+let task1Text = document.getElementById("task1_src").textContent;
+let task1HTML = document.getElementById("task1_dest")
+task1HTML.textContent = task1Text;
+task1HTML.classList.add("green-text");
+
+document.getElementById("task1-heading").className = "royal-blue";
+
+let task2HTML = document.getElementById("task2");
+task2HTML.style.border = "4px solid black";
+task2HTML.style.width = "500px";
+task2HTML.style.backgroundColor = "#C5752A";
+task2HTML.style.color = "white";
+task2HTML.style.textAlign = "center";
+task2HTML.style.fontSize = "2.5em";
+task2HTML.innerText = "JavaScript";
+
+document.querySelector('#task2-heading').className = "royal-blue";
+ 
+document.querySelector('#task2-heading').className = "royal-blue";
+
+document.querySelector(".default-ul li").style.backgroundColor = "red";
+document.getElementById("task3-list-item-3").style.backgroundColor = "red";
+document.getElementById("task3-heading").classList.add("royal-blue");
+
+
+
+
+ 
+
+ 
+ 
