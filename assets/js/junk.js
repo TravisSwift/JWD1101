387932b2d87525d7a0666ec63c1867817f3c2a91
Travis Swift <travis.arthur.swift@gmail.com>
2024-10-20 21:48:41 -0500

    arrays and loops

--- assets/js/junk.js
+++ assets/js/junk.js
@@ -1,59 +1,7 @@
-"use strict"
-
-// Keyword = FUnction
-// Function name
-// () Input
-function myFunction() {
-    let message = "hello world";
-    console.log(message); 
-} 
-
-myFunction();
-
-
- 
-
-function createADog(name, age, breed)
+var numMssg = "";
+for (var i = 0; i < 3; i++)
 {
-let newDog =  {
-    name:name,
-    age:age,
-    breed:breed,
+ numMssg += "The current number is " + i + "." + "<br>";
 }
-let functionScope = "I only live in the function";
-console.log(functionScope);
-return newDog;
- 
-}
+document.write(numMssg);
 
-let someDog = createADog("Honey", 1, "Poodle")
-let anotherDog = createADog("Sam", 2, "Pitbull")
-console.log(someDog);
-console.log(anotherDog);
- 
-
-let myColors =["red", "blue", "green"];
-for(let color of myColors){
-    let paintBucket = {
-        size: "half-gallon",
-        paintcolor: color
-    }
-    console.log(paintBucket); 
-      
-}
-
- for (let color of myColors){
-var paintBucket = {
-    size: "Half-gallon",
-    paintcolor:color
-}
-var artifact = "I should have been cleaned up";
-
-console.log(paintBucket);
-console.log(artifact);
-    
-
- }
-
- 
- 
